508a153413a4e6b54e2f14f104e3adb630416d3f
bnnyng <bonnieyang0131@gmail.com>
2024-11-17 18:12:50 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+df
