6e578ee26fc59e58d51cde9edd436922c3ed7913
bnnyng <bonnieyang0131@gmail.com>
2024-11-18 15:27:34 -0500

    Added sliders and learner optimization

--- app.py
+++ app.py
@@ -11,18 +11,118 @@ import app_functions as F
 
 torch.manual_seed(42)
 
-###  IMPORT FUNCTION DATA  ###
-
-with open('test_stim.json') as json_file:
-    true_function_data = json.load(json_file)
-
-sin_data = true_function_data['sin']
-true_x = torch.arange(0, 6, 0.02)
-true_periods = [2 * math.pi / abs(sin_data[i]['params'][1]) for i in range(len(sin_data))]
+###  INITIAL VALUES  ###
 
 st.title("Visualizing learner parameters")
 
-period = st.slider(label="Period parameter", min_value=0.5, max_value=float(9))
+# Import function data
+with open('test_stim.json') as json_file:
+    true_function_data = json.load(json_file)
+sin_data = true_function_data['sin']
+closed_form_strings = [
+    f'y = {A:<.2f} * sin({B:<.2f} * x - {C:<.2f}) + {D:<.2f}'
+    for i in range(len(sin_data))
+    for (A, B, C, D) in [sin_data[i]['params']]
+]
+
+# Choose training indices
+true_x = torch.arange(0, 6, 0.02)
+train_idx_input = None
+train_idx_string = st.text_input(
+    "Enter training indices as a list of comma-separated integers:",
+    value=None
+)
+try:
+    train_idx_input = train_idx_string.split(',')
+    train_idx_input = [int(n) for n in train_idx_input]
+except:
+    st.write("Oops! Invalid input format.")
+    train_idx_input = None
+train_idxs = np.arange(0, 300) if train_idx_input is None else train_idx_input
+
+# Choose functions
+chosen_function_strings = st.multiselect(
+    "Choose functions to visualize",
+    [f"Function {i}: {closed_form_strings[i]}" for i in range(len(sin_data))]
+)
+chosen_function_indices = [int(fn_str.split()[1][0]) for fn_str in chosen_function_strings]
+
+# Initial optimal inputs
+def get_initial_learner(function_i, n_iter=600):
+    true_period = 2 * math.pi / abs(function_i['params'][1])
+    optim_params = {
+        "sigma": 1,
+        "period": true_period,
+        "length_scale": 1,
+        "noise_sd": 1
+    }
+    optim_learner, _ = F.learner_optim(
+        function_i, true_x, 
+        train_idxs=train_idxs, 
+        params=optim_params,
+        n_iter=n_iter
+    )
+    return optim_learner
+
+def get_initial_optim_params(fn_indices, n_iter=600):
+    optim_params = {}
+    for i in fn_indices:
+        tensor_params = get_initial_learner(
+            sin_data[i], n_iter=n_iter
+        ).gp_distrib.get_kernels()[0].get_params()
+        optim_params[i] = {k: v.item() for k, v in tensor_params.items()}
+    return optim_params
+
+def get_input_params(initial_optim_params):
+    input_params = {}
+    for fn_i, params in initial_optim_params.items():
+        curr_params = {}
+        for k, v in params.items():
+            curr_params[k] = st.slider(
+                label=k,
+                min_value=0.01,
+                max_value=float(9),
+                value=v
+            ) 
+        input_params[fn_i] = curr_params
+    return input_params
+
+initial_optim_params = get_initial_optim_params(chosen_function_indices, n_iter=200)
+
+###  DISPLAY MODULES  ###
+
+def display_function_module(fn, initial_fn_params, plot_title=''):
+    col1, col2 = st.columns(2)
+    input_params = {}
+    with col1:
+        for k, v in initial_fn_params.items():
+            input_params[k] = st.slider(label=k, min_value=0.01, max_value=float(9), value=v)
+    with col2:
+        F.evaluate_params(fn, true_x, train_idxs, input_params, plot_title=plot_title)
+
+for i in chosen_function_indices:
+    true_period = 2 * math.pi / abs(sin_data[i]['params'][1])
+    display_function_module(
+        fn=sin_data[i],
+        initial_fn_params=initial_optim_params[i],
+        plot_title=f"Function {i} with true period {true_period:<.2f}"
+    )
+
+
+
+
+
+
+
+
+
+# Get optimized parameters
+
+
+
+# period = st.slider(label="Period parameter", min_value=0.5, max_value=float(9))
+
+
 
 ###  VISUALIZE ALL FUNCTIONS WITH VARIABLE N POINTS  ###
 
@@ -50,7 +150,7 @@ def visualize_range_n_points():
 
 def visualize_fixed_n_points(fn_list):
     num_noise = st.slider(label="Number of noisy points", min_value=5, max_value=100, step=1)
-    n_cols = 3
+    n_cols = len(fn_list) if len(fn_list) < 3 else 3
     columns = st.columns(n_cols)
     for i, fn in enumerate(fn_list):
         fn_y = fn['all_y']
@@ -65,8 +165,8 @@ def visualize_fixed_n_points(fn_list):
 
 ###  CHOOSE FUNCTION TO VISUALIZE  ###
 
-chosen_fns = st.multiselect(
-    "Choose functions to visualize",
-    [f"Function {i}, true period {true_periods[i]}" for i in range(len(sin_data))]
-)
-visualize_fixed_n_points([sin_data[i] for i in range(len(chosen_fns))])
+# chosen_fns = st.multiselect(
+#     "Choose functions to visualize",
+#     [f"Function {i}, true period {true_periods[i]}" for i in range(len(sin_data))]
+# )
+# visualize_fixed_n_points([sin_data[i] for i in range(len(chosen_fns))])
--- app_functions.py
+++ app_functions.py
@@ -3,6 +3,7 @@ import numpy as np
 import model_torch as model
 import matplotlib.pyplot as plt
 import streamlit as st
+import tqdm
 
 # Function to easily build a new GP Mixture object
 def new_gpm(kernel_params: dict, true_x: list):
@@ -47,74 +48,6 @@ def new_gpm(kernel_params: dict, true_x: list):
 
     return model.GaussianProcessMixture(gps)
 
-
-def evaluate_periodicity(xs, ys, train_idxs, period_samples, plot=True):
-    utilities = np.zeros_like(period_samples)
-    logpriors = np.zeros_like(period_samples)
-    logposteriors = np.zeros_like(period_samples)
-    periods = []
-
-    train_xs = torch.tensor([xs[i].item() for i in train_idxs])
-    train_ys = torch.tensor([ys[i] for i in train_idxs])
-    train_ys += torch.randn_like(train_ys) * .01
-
-    max_logposterior = -float('inf')
-    for i in range(len(period_samples)):
-        period = period_samples[i]
-
-        params = {
-            "sigma": torch.Tensor([1]),
-            "period": torch.Tensor([period]),
-            "length_scale": torch.Tensor([1]),
-            "noise_sd": torch.Tensor([1]),
-            "true_x": torch.Tensor(xs)
-            # "sigma": 1,
-            # "period": period,
-            # "length_scale": 1,
-            # "noise_sd": .05
-        }
-
-        gp_k = new_gpm({"periodic": params}, true_x=xs)
-
-        learner = model.Learner(
-            prior_over_fams={},
-            prior_over_params={},
-            nx=xs[:, None],
-            num_gps=gp_k.num_gps,
-            gp_distrib=gp_k
-        )
-
-        learner_updated = learner.update(x=train_xs[:, None], y=train_ys)
-
-        loglike = learner_updated.log_likelihood()
-        logprior = learner_updated.log_prior()
-        logposterior = loglike + logprior
-
-        utilities[i] = loglike
-        logpriors[i] = logprior
-        logposteriors[i] = logposterior
-        if max_logposterior < logposterior.item():
-            max_logposterior = logposterior.item()
-            best_learner = learner_updated
-
-    best_period = best_learner.gp_distrib.get_kernels()[0].get_params()['periodic_period'].item()
-
-    if plot:
-
-        fig, ax = plot_gp(best_learner, ys, show_known_points=True)
-        fig.suptitle(f'Predictions With {len(train_xs)} Noisy Points')
-        st.pyplot(fig)
-        # print("max logposterior", max_logposterior)
-        # print("max posterior periodicity", best_learner.gp_distrib.get_kernels()[0].get_params()['periodic_period'].item())
-
-        # fig, ax = plt.subplots()
-        # ax.plot(period_samples, logposteriors)
-        # fig.suptitle(f'LogPosterior vs. Periodicity Parameter w/ {len(train_xs)} Noisy Points')
-        # ax.set_xlabel('periodicity parameter')
-        # st.pyplot(fig)
-
-    return utilities, logpriors, logposteriors, best_period
-
 def plot_gp(learner: model.Learner, ys=None, show_known_points=True):
     # Extract known points (training data)?
     known_xs = learner.gp_distrib.gps[0].x.flatten().detach().numpy()
@@ -122,6 +55,9 @@ def plot_gp(learner: model.Learner, ys=None, show_known_points=True):
 
     domain_xs = learner.nx.flatten().detach().numpy()
     test_ys = learner.gp_distrib.mean()
+    # st.write(domain_xs)
+    # st.write(test_ys)
+
     fig, ax = plt.subplots()
     if show_known_points:
         ax.plot(known_xs, known_ys, 'ro')
@@ -136,3 +72,85 @@ def plot_gp(learner: model.Learner, ys=None, show_known_points=True):
         ax.plot(domain_xs, ys, linestyle='--', color='black', linewidth = 3)
         ax.set_ylim(0,3.5)
     return fig, ax
+
+def learner_optim(function_i, true_x, train_idxs, params, n_iter=600):
+    # Get closed form of function
+    A, B, C, D = function_i['params']
+    target_fn = lambda x: A * torch.sin(B * x - C) + D
+
+    train_xs = torch.tensor([true_x[i].item() for i in train_idxs])
+    train_ys = target_fn(train_xs)
+    
+    for k, v in params.items():
+        params[k] = torch.Tensor([v]).requires_grad_(True)
+
+    gp_k = new_gpm({"periodic": params}, train_xs)
+    learner_optimizer = torch.optim.Adam(
+        [v for k, v in params.items() if v.requires_grad],
+        lr=0.01
+    )
+    learner = model.Learner(
+        prior_over_fams= {},
+        prior_over_params={},
+        nx=true_x[:,None],
+        num_gps=gp_k.num_gps,
+        gp_distrib = gp_k
+    )
+        
+    iterator = tqdm.tqdm(range(n_iter))
+    for i in iterator:
+        learner_optimizer.zero_grad()
+        learner_updated = learner.update(x=train_xs[:,None], y=train_ys)
+
+        logpost = learner_updated.log_posterior()
+        (-logpost).backward()
+        # iterator.set_description(
+        #     f"post: {logpost.item():.2f}; pd: {params['period'].item():.2f}; n: {params['noise_sd'].item():.2f}; ls: {params['length_scale'].item():.2f}; s: {params['sigma'].item():.2f}"
+        # )
+        learner_optimizer.step()
+        params['noise_sd'].data.clamp_(0.1, None)
+        params['length_scale'].data.clamp_(0.01, None)
+        params['sigma'].data.clamp_(0.01, None)
+        params['period'].data.clamp_(0.01, None)
+    
+    return learner, train_ys
+
+def evaluate_params(function_i, true_x, train_idxs, params, plot=True, show_known_points=True, plot_title=''):
+    A, B, C, D = function_i['params']
+    target_fn = lambda x: A * torch.sin(B * x - C) + D
+
+    train_xs = torch.tensor([true_x[i].item() for i in train_idxs])
+    train_ys = target_fn(train_xs)
+    # st.write(train_xs)
+    # st.write(train_ys)
+    train_ys += torch.randn_like(train_ys) * .01
+    # train_ys = torch.tensor([ys[i] for i in train_idxs])
+    
+    tensor_params = {
+        'sigma': torch.Tensor([params['periodic_sigma']]),
+        'period': torch.Tensor([params['periodic_period']]),
+        'length_scale': torch.Tensor([params['periodic_length_scale']]),
+        'noise_sd': torch.Tensor([1])
+    }
+
+    gp_k = new_gpm({"periodic": tensor_params}, true_x=true_x)
+
+    learner = model.Learner(
+        prior_over_fams={},
+        prior_over_params={},
+        nx=true_x[:, None],
+        num_gps=gp_k.num_gps,
+        gp_distrib=gp_k
+    )
+    learner_updated = learner.update(x=train_xs[:, None], y=train_ys)
+
+    loglike = learner_updated.log_likelihood()
+    logprior = learner_updated.log_prior()
+    logposterior = loglike + logprior
+
+    if plot:
+        fig, ax = plot_gp(learner_updated, ys=target_fn(true_x), show_known_points=show_known_points)
+        fig.suptitle(plot_title)
+        st.pyplot(fig)
+    
+    return loglike, logprior, logposterior
