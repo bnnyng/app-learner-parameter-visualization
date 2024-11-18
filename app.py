79e31a111bb43495e5766b5d6f29e6d3e55fb74f
bnnyng <bonnieyang0131@gmail.com>
2024-11-17 19:51:27 -0500

    Added slider for number of noise points

--- app.py
+++ app.py
@@ -26,11 +26,12 @@ st.title("Visualizing learner parameters")
 
 
 period = st.slider(label="Period parameter", min_value=0.5, max_value=float(9))
+num_noise = st.slider(label="Number of noisy points", min_value=5, max_value=100, step=1)
 
 def visualize_function_periods(function_data):
     function_y = function_data['all_y']
     true_period = 2 * math.pi / abs(function_data['params'][1])
-    st.subheader(f"True period: {true_period}")
+    st.text(f"True period: {true_period}")
     with st.container():
         n_cols = len(noise_x) // 2
         columns = st.columns(n_cols)
@@ -40,7 +41,15 @@ def visualize_function_periods(function_data):
             with columns[col_n]:
                 F.evaluate_periodicity(true_x, function_y, known_points, [period], plot=True)
 
+# with st.container(height=500):
+#     for fn in sin_data:
+#         visualize_function_periods(fn)
 
-with st.container(height=500):
-    for fn in sin_data:
-        visualize_function_periods(fn)
+columns = st.columns(5)
+for i, fn in enumerate(sin_data):
+    fn_y = fn['all_y']
+    true_period = 2 * math.pi / abs(fn['params'][1])
+    col_n = i % 5
+    known_points = np.random.choice(np.arange(300), num_noise, replace=False)
+    with columns[col_n]:
+        F.evaluate_periodicity(true_x, fn_y, known_points, [period])
